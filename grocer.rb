c67eaa6d22b883f5e5880f7e8ea947bd2f334c15
Tish Chungoora <tishal82@gmail.com>
2019-09-26 15:52:41 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -58,10 +58,8 @@ end
 
 def apply_clearance(cart)
   
-  #Go through each key in the cart hash
+  #Go through each key in the cart hash. If clearance applies to the item then update its price so that a 20% discount is applied, rounded to two decimal places
   cart.each_key do |item|
-    
-    # If clearance applies to the item then update its price so that a 20% discount is applied, rounded to two decimal places
     if cart[item][:clearance] == true
       cart[item][:price] = (cart[item][:price] * 0.8).round(2)
     end
@@ -81,7 +79,7 @@ def checkout(cart, coupons)
   # Initialize the amount due as zero
   amount_due = 0
   
-  # Go through each item in the final cart hash to cummulatively calculate the amount due. The total per item is the product of its price and quantity
+  # Go through each item in the final cart hash to cumulatively calculate the amount due. The total per item is the product of its price and quantity
   final_cart.each do |item, attributes|
     amount_due += attributes[:price] * attributes[:count]
     
